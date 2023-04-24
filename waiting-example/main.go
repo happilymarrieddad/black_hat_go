0f0659f7923be27d55a26a3220cc9bb0d3f22e7e
Nick Kotenberg <happilymarrieddad@yahoo.com>
2023-04-24 12:18:38 -0600

    Update main.go

--- waiting-example/main.go
+++ waiting-example/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"sync"
 	"time"
 )
+
+type ErrHandler struct {
+	Errs []error
+}
 
 type Task struct {
 	ID   int
@@ -13,17 +18,26 @@ type Task struct {
 
 type TaskChannel chan Task
 
-func worker(taskChannel TaskChannel, wg *sync.WaitGroup) {
+func worker(taskChannel TaskChannel, wg *sync.WaitGroup, errHandler *ErrHandler) {
 	fmt.Println("Worker started")
 	for task := range taskChannel {
 		fmt.Printf("Starting task %d\n", task.ID)
 
 		time.Sleep(time.Second * 2)
+		if task.ID == 2 {
+			fmt.Println("Added err")
+			errHandler.Errs = append(errHandler.Errs, errors.New("some error"))
+		}
+		if task.ID == 10 {
+			fmt.Println("Added err")
+			errHandler.Errs = append(errHandler.Errs, errors.New("some error 2"))
+		}
 		wg.Done()
 	}
 }
 
 func main() {
+	errHandler := new(ErrHandler)
 	wg := &sync.WaitGroup{}
 	numWorkers := 5
 	taskChannel := make(TaskChannel)
@@ -37,7 +51,7 @@ func main() {
 	wg.Add(len(data))
 
 	for i := 0; i < numWorkers; i++ {
-		go worker(taskChannel, wg)
+		go worker(taskChannel, wg, errHandler)
 	}
 
 	// This is just to delay so the workers are started and ready
@@ -50,5 +64,13 @@ func main() {
 	wg.Wait()
 	close(taskChannel)
 
+	var err error
+	for _, e := range errHandler.Errs {
+		fmt.Println("handling err")
+		err = errors.Join(err, e)
+	}
+
 	fmt.Println("Finished!!")
+	fmt.Println("With errs: ")
+	fmt.Println(err.Error())
 }
